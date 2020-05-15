f0ab762e590be7968a05a74370e1d343415855e4
wyx <910599247@qq.com>
2020-05-15 15:29:52 +0800

    add:Https get请求抓包

--- app/src/main/java/com/example/HttpConnection/JsonActivity.java
+++ app/src/main/java/com/example/HttpConnection/JsonActivity.java
@@ -24,10 +24,19 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class JsonActivity extends AppCompatActivity implements View.OnClickListener {
     private final String Tag = "JsonActivity";
@@ -60,12 +69,61 @@ public class JsonActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    private String get(String url) {
+    // always verify the host - dont check for certificate
+    final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    };
+
+    /**
+     * Trust every server - dont check for any certificate
+     */
+    private static void trustAllHosts() {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[]{};
+            }
+
+            public void checkClientTrusted(X509Certificate[] chain,
+                                           String authType) throws CertificateException {
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain,
+                                           String authType) throws CertificateException {
+            }
+        }};
+
+        // Install the all-trusting trust manager
         try {
-//      实例化管理网络地址的对象
-            URL Url = new URL(url);
-//      实例化管理网络连接的对象,设置timeout和get方法
-            HttpURLConnection conn = (HttpURLConnection) Url.openConnection();
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection
+                    .setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private String get(String address) {
+        try {
+//            实例化URL对象，URL
+            URL url = new URL(address);
+
+            HttpURLConnection conn = null;
+
+            if (url.getProtocol().toLowerCase().equals("https")) {
+                trustAllHosts();
+                HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
+                https.setHostnameVerifier(DO_NOT_VERIFY);
+                conn = https;
+            } else {
+                conn = (HttpURLConnection) url.openConnection();
+            }
+
             conn.setConnectTimeout(6000);
             conn.setRequestMethod("GET");
 
